1f2364dcca692f978ed1f71a742911da7d5900d8
Mugabo Andre <andremugabo@yahoo.fr>
2026-09-19 23:28:49 +0200

    feat: patch and delete the post

--- backend/src/controller/post.controller.js
+++ backend/src/controller/post.controller.js
@@ -11,24 +11,58 @@ const createPost = async (req, res) => {
         message: "All field are required",
       });
     }
-      const post = await Post.create({ name, description, age });
-      res.status(201).json({
-        message: "Post create successfully",
-        post,
-      });
-    
+    const post = await Post.create({ name, description, age });
+    res.status(201).json({
+      message: "Post create successfully",
+      post,
+    });
   } catch (error) {
     res.status(500).json({ message: "Interanl Server error", error });
   }
 };
 
-const getPosts =  async (req, res) =>{
-    try {
-        
-    } catch (error) {
-        
+const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server error",
+      error,
+    });
+  }
+};
+
+const updatePost = async (req, res) => {
+  try {
+    // Basic validation to check if the body is empty
+    // {name: x, description: y, age: z} -> [name, description, age]
+    // {empty} = truthy
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).json({
+        message: "No data provided for update",
+      });
     }
-}
+    const post = await Post.findById(req.params.id, req.body, { new: true });
+    if (!post) return res.status(404).json({ message: "Post not found" });
+    res.status(200).json({ message: "Post Update Successfully ", post });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error ", error });
+  }
+};
 
+const deletePost = async (req, res) => {
+  try {
+    const deleted = await Post.findByIdAndDelete(req.params.id);
 
-export { createPost };
+    if (!deleted) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    res.status(200).json({ message: "Post deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Intrnal server error", error });
+  }
+};
+
+export { createPost, getPosts, updatePost, deletePost };
--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { createPost } from "../controller/post.controller.js";
+import {
+  createPost,
+  deletePost,
+  getPosts,
+  updatePost,
+} from "../controller/post.controller.js";
 
 const router = Router();
 
 router.post("/create", createPost);
+router.get("/getPosts", getPosts);
+router.patch("/update/:id", updatePost);
+router.delete("/delete/:id", deletePost);
 
 export default router;
