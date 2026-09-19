a3709f134ef483dd4595786eac67a558186a34ff
Mugabo Andre <andremugabo@yahoo.fr>
2026-09-19 17:06:38 +0200

    login functionality

---
+++ backend/src/controller/user.controller.js
@@ -0,0 +1,70 @@
+import { User } from "../models/user.model.js";
+
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are important! " });
+    }
+
+    // check if user exists already
+
+    const existing = await User.findOne({ email: email.toLowerCase() });
+    if (existing) {
+      return res.status(400).json({ message: "user already exists !" });
+    }
+
+    // create user
+
+    const user = await User.create({
+      username,
+      email: email.toLowerCase(),
+      password,
+      loggedIn: false,
+    });
+
+    res.status(201).json({
+      message: "User registered succesfully !",
+      user: { id: user._id, email: user.email, username: user.username },
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // check if user exists
+    const user = await User.findOne({ email: email.toLowerCase() });
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found " });
+    }
+
+    // compare password
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch)
+      return res.status(404).json({
+        message: "Invalid credentials",
+      });
+
+    res.status(200).json({
+      message: "User logged in ",
+      user: {
+        id: user._id,
+        email: user.email,
+        username: user.username,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: " Internal Server Error ",
+    });
+  }
+};
+
+export { registerUser, loginUser };
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      minLength: 3,
+      maxLength: 30,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minLenght: 1,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// before savving any password we need to hash it
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
